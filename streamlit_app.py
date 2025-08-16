327710d09e5e3afd29f0430c8480fa15017a6b61
Krishna Apil Chowdary Morampudi <krishna.morampudi@kasmodigital.com>
2025-08-16 15:36:23 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,32 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Orders :cup_with_straw: ")
+st.write(
+  """Current pending orders
+  """
+)
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.orders").filter(col('ORDER_FILLED')==False).collect()
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    #st.dataframe(data=my_dataframe, use_container_width=True)
+    
+    submitted=st.button('Submit')
+    if submitted:
+        
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                             , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                             , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                            )
+            st.success('Order(s) updated',icon="👍")
+        except:
+            st.warning('OOPS something went wrong', icon='🤭')
+else:
+    st.success('No pending orders', icon='🤷‍♂️')
