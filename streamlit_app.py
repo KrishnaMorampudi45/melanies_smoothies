f30c22f81d3c3130b5e3fad28329ed3e463300d5
Krishna Apil Chowdary Morampudi <krishna.morampudi@kasmodigital.com>
2025-08-16 20:27:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,17 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()     
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 ingredient_list=st.multiselect('Choose upto 5 ingredients:',my_dataframe,max_selections=5)
 if ingredient_list:
     
     ingredients_string=''
     for i in ingredient_list:
         ingredients_string+=i+' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', i,' is ', search_on, '.')
         st.subheader(i+'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
