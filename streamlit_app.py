2377941e14ab10eec94f1de823cce04cf118a967
Krishna Apil Chowdary Morampudi <krishna.morampudi@kasmodigital.com>
2025-08-16 16:36:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,8 @@ if ingredient_list:
     ingredients_string=''
     for i in ingredient_list:
         ingredients_string+=i+' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(i+'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+i)
         sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_smoothie)
